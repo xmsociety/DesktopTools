c32200720b4f2c76fa49e7b674e39835e585163c
Ian Pi <ianvzs@outlook.com>
2021-04-06 22:34:12 +0800

    fmt

--- monitor.py
+++ monitor.py
@@ -6,6 +6,7 @@ from PySide2.QtCore import QThread, Signal
 from logger import logger
 from data_alchemy.inputs import add_count_keymouse
 
+
 class WorkDict:
     start_time = time.time()
     last_time = time.time()
@@ -13,6 +14,7 @@ class WorkDict:
     rest_time = 0
     noinput_time = 0
     buffer_time = 10
+
     def count_all(self):
         self.work_all = int(self.last_time - self.start_time)
         self.noinput_time = 0
@@ -22,6 +24,7 @@ class WorkDict:
         self.buffer_time -= 1
         if self.buffer_time <= 0:
             self.rest_time += 1
+
 
 class ThreadSimple(QThread):
     # 简易线程
@@ -34,9 +37,11 @@ class ThreadSimple(QThread):
             print(i)
             time.sleep(0.5)  # 休眠
 
+
 class ThreadSignal(QThread):
     # 有信号发出的线程,接受者可根据信号执行任务
     _signal = Signal()
+
     def __init__(self):
         super().__init__()
 
@@ -47,17 +52,17 @@ class ThreadSignal(QThread):
             time.sleep(0.5)
         self._signal.emit()
 
+
 class SignalKeyboard(QThread):
     # 有信号发出的线程,接受者可根据信号执行任务
     _signal = Signal()
+
     def __init__(self):
         super().__init__()
 
     def listen(self):
-        listener = keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release
-        )
+        listener = keyboard.Listener(on_press=self.on_press,
+                                     on_release=self.on_release)
         listener.start()
 
     def log_event(self, name, key, save=0):
@@ -85,16 +90,15 @@ class SignalKeyboard(QThread):
 
 class SignalMouse(QThread):
     _signal = Signal()
+
     def __init__(self):
         self.x, self.y = 0, 0
         super().__init__()
 
     def listen(self):
-        listener = mouse.Listener(
-            on_move=self.on_move,
-            on_click=self.on_click,
-            on_scroll=self.on_scroll
-        )
+        listener = mouse.Listener(on_move=self.on_move,
+                                  on_click=self.on_click,
+                                  on_scroll=self.on_scroll)
         listener.start()
 
     def on_move(self, x, y):
@@ -113,4 +117,3 @@ class SignalMouse(QThread):
         sign = add_count_keymouse("scroll", 0)
         logger.debug(f"scroll&({dx},{dy})&({x},{y})&save_{sign}")
         self._signal.emit()
-
