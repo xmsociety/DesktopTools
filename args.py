abc107b6a3b697dd55c51c0d5fb9b3e137db4ffb
Ian <ianvzs@outlook.com>
2021-04-09 17:58:17 +0800

    add stage continued record.

--- args.py
+++ args.py
@@ -1,6 +1,21 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--debug", type=int, choices=[0, 1], help="debug? 0/1")
+parser.add_argument("--debug",
+                    type=int,
+                    default=0,
+                    choices=[0, 1],
+                    help="debug? 0/1")
+parser.add_argument("--localtime",
+                    default=True,
+                    type=bool,
+                    choices=[True, False],
+                    help="use localtime? True/False")
 
 args = parser.parse_args()
+
+KEYBOARD, MOUSE = "keyboard", "mouse"
+
+KEYBOARD_DeviceNo = 1
+MOUSE_DeviceNo = 0
+TICKER_DeviceNo = -1
